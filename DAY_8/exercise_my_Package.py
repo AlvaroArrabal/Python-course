692117b52a2f612dcdfacaef0513ff6b943968a2
Alvaro <arrabalalvaro@gmail.com>
2022-08-04 13:20:07 +0200

    Second Part DAY 8

---
+++ DAY_8/exercise_my_Package.py
@@ -0,0 +1,7 @@
+from my_Package import add_sub
+from my_Package.sub_Package import hello
+
+add_sub.addition(10,4)
+add_sub.subtraction(20,12)
+
+hello.salute()
---
+++ DAY_8/my_Package/sub_Package/hello.py
@@ -0,0 +1,2 @@
+def salute():
+    print("Hey!")
