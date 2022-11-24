02b25ea68ca232cb12becf3c65e944613d3b6145
AlvaroArrabal <arrabalalvaro@gmail.com>
2022-11-24 23:02:18 +0100

    changing the background

--- DAY_10/main.py
+++ DAY_10/main.py
@@ -11,11 +11,11 @@ screen = pygame.display.set_mode((800, 600))        # (800,600) the size of the 
 # Title and icon
 
 pygame.display.set_caption("Alvaro's Game")         # Sets the title of the programme
-icon = pygame.image.load(".\\DAY_10\\smiley.png")   # load the icon we want. In 32px!
 
+icon = pygame.image.load(".\\DAY_10\\smiley.png")   # load the icon we want. In 32px!
 pygame.display.set_icon(icon)                       # Sets the icon of the programme
 
-running = True
+background = pygame.image.load(".\\DAY_10\\background.png")
 
 # Player variables
 player_icon = pygame.image.load(".\\DAY_10\\spacecraft.png")    # 64px
@@ -31,7 +31,7 @@ def player(x,y):
 Enemy_icon = pygame.image.load(".\\DAY_10\\enemy.png")    # 64px
 Enemy_pos_x = random.randint(0,736)
 Enemy_pos_y = random.randint(50,200)
-Enemy_pos_x_change = 0.1
+Enemy_pos_x_change = 0.2
 Enemy_pos_y_change = 50
 
 # Player function
@@ -43,9 +43,13 @@ def enemy(x,y):
     screen.blit(Enemy_icon,(x,y))
 
 # Game loop
+running = True
+
 while running:
-    screen.fill((46,30,134))              # RGB for the color of the screen
+    #screen.fill((46,30,134))              # RGB for the color of the screen
     # The order of the functions is very important
+    screen.blit(background, (0,0))
+
 
     for event in pygame.event.get():
 
@@ -54,9 +58,9 @@ while running:
 
         if event.type == pygame.KEYDOWN:    # KEYDOWM, when the player press the key.
             if event.key == pygame.K_LEFT:
-                player_pos_change = -0.3
+                player_pos_change = -0.5
             if event.key == pygame.K_RIGHT:
-                player_pos_change = 0.3
+                player_pos_change = 0.5
 
         if event.type == pygame.KEYUP:      # KEYUP when the player releases the key
             if event.key == pygame.K_LEFT:
@@ -78,10 +82,10 @@ while running:
 
     # Inside the screen - enemy
     if Enemy_pos_x <= 0: 
-        Enemy_pos_x_change = 0.1
+        Enemy_pos_x_change = 0.2
         Enemy_pos_y += Enemy_pos_y_change
     elif Enemy_pos_x >= 736:        # 800 - 64 = 736
-        Enemy_pos_x_change = -0.1
+        Enemy_pos_x_change = -0.2
         Enemy_pos_y += Enemy_pos_y_change
     player(player_pos_x,player_pos_y)
     enemy(Enemy_pos_x,Enemy_pos_y)
