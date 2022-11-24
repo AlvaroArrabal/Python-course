216b7261d7dfa276bc11e3189398a6698cfcfa4a
AlvaroArrabal <arrabalalvaro@gmail.com>
2022-11-24 20:47:36 +0100

    title, icon and screen color

--- DAY_10/main.py
+++ DAY_10/main.py
@@ -1,9 +1,19 @@
 import pygame
 
 
-pygame.init()               # start the pygame
+# Initialise pygame
+pygame.init()               
 
+# Create the screen
 screen = pygame.display.set_mode((800, 600))        # (800,600) the size of the screen in pixels
+
+
+# Title and icon
+
+pygame.display.set_caption("Alvaro's Game")         # Sets the title of the programme
+icon = pygame.image.load(".\\DAY_10\\smiley.png")   # load the icon we want. In 32px!
+
+pygame.display.set_icon(icon)                       # Sets the icon of the programme
 
 running = True
 
@@ -11,3 +21,5 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:       # when the user presses the "X" to close windows, the programme will close
             running = False
+    screen.fill((205,144,228))              # RGB for the color of the screen
+    pygame.display.update()                 # update 
