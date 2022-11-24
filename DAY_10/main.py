560622b693aa1ac21f509ce617d80ec7eade2580
AlvaroArrabal <arrabalalvaro@gmail.com>
2022-11-24 21:02:22 +0100

    creating the player

--- DAY_10/main.py
+++ DAY_10/main.py
@@ -17,9 +17,25 @@ pygame.display.set_icon(icon)                       # Sets the icon of the progr
 
 running = True
 
+player_icon = pygame.image.load(".\\DAY_10\\spacecraft.png")    # 64px
+player_pos_x = 368            # 800/2 = 400 - 64/2 = 368
+player_pos_y = 536            # 600-64 = 536
+
+
+def player():
+    screen.blit(player_icon,(player_pos_x,player_pos_y))
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:       # when the user presses the "X" to close windows, the programme will close
             running = False
     screen.fill((205,144,228))              # RGB for the color of the screen
+
+    # The order of the functions is very important
+
+    player()
+
+
     pygame.display.update()                 # update 
+
+    
