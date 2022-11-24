5839ac21223b2da02a16f725cb565d3da31bb625
AlvaroArrabal <arrabalalvaro@gmail.com>
2022-11-24 21:20:15 +0100

    player movement

--- DAY_10/main.py
+++ DAY_10/main.py
@@ -20,20 +20,31 @@ running = True
 player_icon = pygame.image.load(".\\DAY_10\\spacecraft.png")    # 64px
 player_pos_x = 368            # 800/2 = 400 - 64/2 = 368
 player_pos_y = 536            # 600-64 = 536
+player_pos_change = 0
 
-
-def player():
-    screen.blit(player_icon,(player_pos_x,player_pos_y))
+def player(x,y):
+    screen.blit(player_icon,(x,y))
 
 while running:
+    screen.fill((205,144,228))              # RGB for the color of the screen
+    # The order of the functions is very important
     for event in pygame.event.get():
         if event.type == pygame.QUIT:       # when the user presses the "X" to close windows, the programme will close
             running = False
-    screen.fill((205,144,228))              # RGB for the color of the screen
 
-    # The order of the functions is very important
-
-    player()
+        if event.type == pygame.KEYDOWN:    # KEYDOWM, when the player press the key. KEYUP when the player releases the key
+            if event.key == pygame.K_LEFT:
+                player_pos_change = -0.3
+            if event.key == pygame.K_RIGHT:
+                player_pos_change = 0.3
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                player_pos_change = 0
+            if event.key == pygame.K_RIGHT:
+                player_pos_change = 0
+    
+    player_pos_x += player_pos_change
+    player(player_pos_x,player_pos_y)
 
 
     pygame.display.update()                 # update 
