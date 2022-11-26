c9bc0becc59dbd35b7ca5deec98c008c91098bc3
AlvaroArrabal <arrabalalvaro@gmail.com>
2022-11-26 19:50:21 +0100

    First commit day 11 - creating an App

---
+++ DAY_11/main.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+# Initiating tkinder
+app = Tk()
+
+# dimension of the window
+app.geometry("1020x630+0+0")
+app.resizable(0,0) # The user cant change the dimension if the window
+
+app.title("My restaurant") # change the name of the window
+app.config(bg="burlywood")
+
+app.mainloop() # The programme doesnt close
+
