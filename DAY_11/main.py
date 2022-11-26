4a3107bb402f8cdd287ef2ca9928af2143a350b6
AlvaroArrabal <arrabalalvaro@gmail.com>
2022-11-26 21:23:35 +0100

    panel cost

--- DAY_11/main.py
+++ DAY_11/main.py
@@ -27,7 +27,7 @@ leftPanel = Frame(app,bd =1,relief=FLAT)
 leftPanel.pack(side=LEFT)
 
 # Cost panel
-costPanel = Frame(app,bd =1,relief=FLAT)
+costPanel = Frame(leftPanel,bd =1,relief=FLAT,bg="azure4",padx=55)
 costPanel.pack(side=BOTTOM)
 
 # Food panel
@@ -59,9 +59,9 @@ buttonsPanel = Frame(app,bd =1,relief=FLAT,bg="burlywood")
 buttonsPanel.pack(side=RIGHT)
 
 # list of products
-foodList = ["chicken","potatoes","salmon","burger","pasta","pizza1","pizza2","pizza3","pizza4"]
-drinksList = ["water","Coca Cola","beer1","beer2","beer3","wine1","wine2","wine3","wine4"]
-dessertsList = ["ice cream","fruit","brownie","mousse","cake1","cake2","cake3","cake4","cake5"]
+foodList = ["chicken","potatoes","salmon","burger","pasta","pizza1","pizza2","pizza3"]
+drinksList = ["water","Coca Cola","beer1","beer2","beer3","wine1","wine2","wine3"]
+dessertsList = ["ice cream","fruit","brownie","mousse","cake1","cake2","cake3","cake4"]
 
 # Generating food items
 foodVariables= []
@@ -133,6 +133,68 @@ for desserts in dessertsList:
     dessertsQuantity[cont].grid(row=cont,column=3)
 
     cont +=1
+
+# Total cost
+subtotal = StringVar()
+taxes = StringVar()
+total = StringVar()
+
+# food
+foodCost = Label(costPanel,text="Food Cost", font=("Dosis",14,"bold"), bg="azure4",fg="white")
+foodCost.grid(row=0,column=0)
+
+costFoodVar = StringVar()
+
+foodCostText = Entry(costPanel,font=("Dosis",14,"bold"),bd=1,width=10,state="readonly",textvariable=costFoodVar)
+foodCostText.grid(row=0,column=1, padx=35)
+
+# drinks
+drinksCost = Label(costPanel,text="Drinks Cost", font=("Dosis",14,"bold"), bg="azure4",fg="white")
+drinksCost.grid(row=1,column=0)
+
+costDrinksVar = StringVar()
+
+drinksCostText = Entry(costPanel,font=("Dosis",14,"bold"),bd=1,width=10,state="readonly",textvariable=costDrinksVar)
+drinksCostText.grid(row=1,column=1, padx=35)
+
+# desserts
+dessertsCost = Label(costPanel,text="Desserts Cost", font=("Dosis",14,"bold"), bg="azure4",fg="white")
+dessertsCost.grid(row=2,column=0)
+
+costDessertsVar = StringVar()
+
+dessertsCostText = Entry(costPanel,font=("Dosis",14,"bold"),bd=1,width=10,state="readonly",textvariable=costDessertsVar)
+dessertsCostText.grid(row=2,column=1, padx=35)
+
+# subtotal
+subtotal = Label(costPanel,text="Subtotal", font=("Dosis",14,"bold"), bg="azure4",fg="white")
+subtotal.grid(row=0,column=2)
+
+subtotalVar = StringVar()
+
+subtotalText = Entry(costPanel,font=("Dosis",14,"bold"),bd=1,width=10,state="readonly",textvariable=subtotalVar)
+subtotalText.grid(row=0,column=3, padx=35)
+
+# taxes
+taxes = Label(costPanel,text="Taxes", font=("Dosis",14,"bold"), bg="azure4",fg="white")
+taxes.grid(row=1,column=2)
+
+taxesVar = StringVar()
+
+taxesText = Entry(costPanel,font=("Dosis",14,"bold"),bd=1,width=10,state="readonly",textvariable=taxesVar)
+taxesText.grid(row=1,column=3, padx=35)
+
+# total
+total = Label(costPanel,text="Total", font=("Dosis",14,"bold"), bg="azure4",fg="white")
+total.grid(row=2,column=2)
+
+totalVar = StringVar()
+
+totalText = Entry(costPanel,font=("Dosis",14,"bold"),bd=1,width=10,state="readonly",textvariable=totalVar)
+totalText.grid(row=2,column=3, padx=35)
+
+
+
 # The programme doesnt close
 app.mainloop() 
 
