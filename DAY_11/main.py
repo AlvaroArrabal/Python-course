960586f1e1c26e36ede8981597e617d03edc663d
AlvaroArrabal <arrabalalvaro@gmail.com>
2022-12-06 20:11:54 +0100

    receipt generator

--- DAY_11/main.py
+++ DAY_11/main.py
@@ -1,6 +1,12 @@
 from tkinter import *
+import random
+import datetime
 
 operator = ""
+
+foodPrices = [1.32, 1.65, 2.31, 3.22, 1.22, 1.99, 2.05, 2.65]
+drinksPrices = [0.25, 0.99, 1.21, 1.54, 1.08, 1.10, 2.00, 1.58]
+dessertsPrices = [1.54, 1.68, 1.32, 1.97, 2.55, 2.14, 1.94, 1.74]
 
 def press_button(number):
     global operator
@@ -20,8 +26,83 @@ def result():
     screen.insert(0,res)
     operator = ""
 
+def check_button():
+    cont = 0
+    for i in foodQuantity:
+        if foodVariables[cont].get() == 1:
+            foodQuantity[cont].config(state=NORMAL)
+            if foodQuantity[cont].get() == "0":
+                foodQuantity[cont].delete(0,END)
+                foodQuantity[cont].focus()
+        else:
+            foodQuantity[cont].config(state=DISABLED)
+            foodText[cont].set("0")
+        cont += 1
 
+    cont = 0
+    for i in drinksQuantity:
+        if drinksVariables[cont].get() == 1:
+            drinksQuantity[cont].config(state=NORMAL)
+            if drinksQuantity[cont].get() == "0":
+                drinksQuantity[cont].delete(0,END)
+                drinksQuantity[cont].focus()
+        else:
+            drinksQuantity[cont].config(state=DISABLED)
+            drinksText[cont].set("0")
+        cont += 1
+
+    cont = 0
+    for i in dessertsQuantity:
+        if dessertsVariables[cont].get() == 1:
+            dessertsQuantity[cont].config(state=NORMAL)
+            if dessertsQuantity[cont].get() == "0":
+                dessertsQuantity[cont].delete(0,END)
+                dessertsQuantity[cont].focus()
+        else:
+            dessertsQuantity[cont].config(state=DISABLED)
+            dessertsText[cont].set("0")
+        cont += 1
+
+def calculate_total():
+    subtotalFood = 0
+    subtotalDrink = 0
+    subtotalDessesrt = 0
+    cont = 0
+    for i in foodText:
+        subtotalFood = subtotalFood + (float(i.get()) * foodPrices[cont])
+        cont += 1
     
+    cont = 0
+    for i in drinksText:
+        subtotalDrink = subtotalDrink + (float(i.get()) * drinksPrices[cont])
+        cont += 1
+    
+    cont = 0
+    for i in dessertsText:
+        subtotalDessesrt = subtotalDessesrt + (float(i.get()) * dessertsPrices[cont])
+        cont += 1
+    
+    subtotal = subtotalFood + subtotalDrink + subtotalDessesrt
+    taxes = 0.21*subtotal
+    total = subtotal + taxes
+
+    costFoodVar.set(f"€ {round(subtotalFood,2)}")
+    costDrinksVar.set(f"€ {round(subtotalDrink,2)}")
+    costDessertsVar.set(f"€ {round(subtotalDessesrt,2)}")
+    subtotalVar.set(f"€ {round(subtotal,2)}")
+    taxesVar.set(f"€ {round(taxes,2)}")
+    totalVar.set(f"€ {round(total,2)}")
+
+def generate_receipt():
+    receiptText.delete(1.0 ,END)
+    receiptNumber = f"N# - {random.randint(1000,9999)}"
+    date = datetime.datetime.now()
+    receiptDate = f"{date.day}/{date.month}/{date.year} - {date.hour}:{date.hour}"
+    receiptText.insert(END, f"Data: \t{receiptNumber}\t\t\t{receiptDate}\n")
+    receiptText.insert(END,"*"*70 + "\n")
+    receiptText.insert(END,"Items\t\t\tNum\t\tCost\n")
+    receiptText.insert(END,"-"*87 + "\n")
+
 # Initiating tkinder
 app = Tk()
 
@@ -95,7 +176,7 @@ for food in foodList:
     # Check Buttons
     foodVariables.append("")
     foodVariables[cont] = IntVar()
-    food = Checkbutton(foodPanel,text=food.title(),font=("Dosis",19,"bold"),onvalue=1,offvalue=0, variable=foodVariables[cont])
+    food = Checkbutton(foodPanel,text=food.title(),font=("Dosis",19,"bold"),onvalue=1,offvalue=0, variable=foodVariables[cont],command=check_button)
     food.grid(row=cont, column=0, sticky=W)  
     
     # Food Quantity
@@ -119,7 +200,7 @@ for drinks in drinksList:
     # Check Buttons
     drinksVariables.append("")
     drinksVariables[cont] = IntVar()
-    drinks = Checkbutton(drinksPanel,text=drinks.title(),font=("Dosis",19,"bold"),onvalue=1,offvalue=0, variable=drinksVariables[cont])
+    drinks = Checkbutton(drinksPanel,text=drinks.title(),font=("Dosis",19,"bold"),onvalue=1,offvalue=0, variable=drinksVariables[cont],command=check_button)
     drinks.grid(row=cont , column=1, sticky=W)  
     
     # drinks Quantity
@@ -143,7 +224,7 @@ for desserts in dessertsList:
     # Check Buttons
     dessertsVariables.append("")
     dessertsVariables[cont] = IntVar()
-    desserts = Checkbutton(dessertsPanel,text=desserts.title(),font=("Dosis",19,"bold"),onvalue=1,offvalue=0, variable=dessertsVariables[cont])
+    desserts = Checkbutton(dessertsPanel,text=desserts.title(),font=("Dosis",19,"bold"),onvalue=1,offvalue=0, variable=dessertsVariables[cont],command=check_button)
     desserts.grid(row=cont , column=2, sticky=W)  
 
     # desserts Quantity
@@ -217,12 +298,17 @@ totalText.grid(row=2,column=3, padx=35)
 
 # Buttons
 buttons = ["Total", "Receipt", "Save", "Reset"]
+createdButtons = []
 columns = 0
 
 for button in buttons:
     button = Button(buttonsPanel, text=button.title(),font=("Dosis",11,"bold"),fg="white",bg="azure4",bd=1,width=9)
+    createdButtons.append(button)
     button.grid(row=0,column=columns)
     columns += 1
+
+createdButtons[0].config(command = calculate_total)
+createdButtons[1].config(command = generate_receipt)
 
 # Receipt Area
 receiptText = Text(receiptPanel,font=("Dosis",10,"bold"),fg="black",bd=1,width=50,height=15)
